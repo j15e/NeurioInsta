1baa4c34b0e6623fbed255a8d9df68736b72f65c
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2016-09-03 20:47:02 -0400

    First commit

---
+++ NeurioInsta.ino
@@ -0,0 +1,121 @@
+#include "NeurioInstaEnv.h"
+
+#include <Adafruit_LEDBackpack.h>
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+
+Adafruit_7segment matrix = Adafruit_7segment();
+
+int load_time = 0;
+int max_pw_read = 0;
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+
+  // Connecting to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_NAME);
+
+  WiFi.begin(WIFI_NAME, WIFI_PASSWORD);
+
+  // Set display matrix address
+  matrix.begin(MATRIX_ADDR);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    showLoading(matrix, load_time);
+    load_time++;
+    delay(250);
+  }
+
+  showReady(matrix);
+
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  WiFiClient client;
+  const uint16_t httpPort = 80;
+
+  if (!client.connect(NEURIO_IP, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  // This will send the request to the server
+  client.print(String("GET /current-sample") + " HTTP/1.1\r\n" +
+               "Host: " + NEURIO_IP + "\r\n" +
+               "Connection: close\r\n\r\n");
+
+  // Read all the lines of the reply from server and print them to Serial
+  String response = "";
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    // Get only the JSON body, skip headers
+    if(line.startsWith("\n{")) {
+      response = line;
+    }
+  }
+
+  StaticJsonBuffer<2048> jsonBuffer;
+  JsonObject& sample = jsonBuffer.parseObject(response);
+
+  if (!sample.success()) {
+    Serial.println("JSON parsing failed!");
+    Serial.println("Response body :");
+    Serial.println(response);
+    return;
+  }
+
+  int consumption = sample["channels"][2]["p_W"];
+
+  // Track maximum consumption peak
+  if(consumption > max_pw_read) {
+    max_pw_read = consumption;
+  }
+
+  // Display current consumption & set brightness relative to maximum consumption
+  matrix.setBrightness((float)consumption / max_pw_read * 15);
+  matrix.print(consumption);
+  matrix.writeDisplay();
+  delay(200);
+}
+
+// Show loading screen
+// [=   ]
+// [ =  ]
+// [  = ]
+// [   =]
+void showLoading(Adafruit_7segment matrix, int load_time){
+  int displayDigit = (load_time % 4);
+  int digit = 0;
+
+  // Skip display colon
+  if(displayDigit > 1) displayDigit++;
+
+  while(digit < 5) {
+    if(digit == displayDigit) {
+      matrix.writeDigitRaw(digit, 0x49);
+    } else {
+      matrix.writeDigitRaw(digit, 0x00);
+    }
+    digit++;
+   }
+
+   matrix.writeDisplay();
+}
+
+// Show ready screen [====]
+void showReady(Adafruit_7segment matrix){
+  matrix.writeDigitRaw(0, 0x49);
+  matrix.writeDigitRaw(1, 0x49);
+  matrix.writeDigitRaw(3, 0x49);
+  matrix.writeDigitRaw(4, 0x49);
+  matrix.writeDisplay();
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# NeurioInsta
+
+> Display electricity consumption from your Neurio with ESP8266 & 7-segments display
+
+## What it looks like
+
+![neurio-iot](http://j15e.com/NeurioInsta/device.png)
+
+## Requirements
+
+This is the setup I use, but any ESP8266 Wifi board should work :
+
+- [Adafruit Feather HUZZAH with ESP8266 WiFi](https://www.adafruit.com/products/2821)
+- [Adafruit 0.56" 4-Digit 7-Segment Display w/I2C Backpack](https://www.adafruit.com/products/1002)
+
+## Quick start
+
+Copy the exemple configuration in your project :
+
+   cp NeurioInstaEnv.h.exemple NeurioInstaEnv.h
+
+Define your Wifi settings & Neurip IP in this file.
